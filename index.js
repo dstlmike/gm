479abfe3db094eb86be9eb2f67f948f75effcf5a
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-23 22:39:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,16 +36,17 @@ server = http.createServer(function (req, res) {
 });
 
 port = Number(process.env.PORT || 8080);
-ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+ip = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0" || "127.0.0.1";
 
-server.listen(port, ipAddr, function(req, res) {
-  ipAddr = req.headers["x-forwarded-for"]; 
+server.listen(port, ip, function(req, res) {
+/*  ipAddr = req.headers["x-forwarded-for"]; 
 if (ipAddr){ 
   var list = ipAddr.split(","); 
   ipAddr = list[list.length-1]; 
 } else { 
   ipAddr = req.connection.remoteAddress; 
 }
+*/
   console.log('Server started on port: ' + port + ', IP: ' + ipAddr)
 });
 
