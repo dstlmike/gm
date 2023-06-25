b00023b6c59458402174335323d17ef4f3e81dae
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-25 19:52:14 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -44,7 +44,7 @@ MongoClient.connect(uri, function(err, client) {
 var MongoClient = require('mongodb').MongoClient;
 
 var uri = "mongodb://dstlmike1:308boonave@ac-cbqypvj-shard-00-00.f8ibx0h.mongodb.net:27017,ac-cbqypvj-shard-00-01.f8ibx0h.mongodb.net:27017,ac-cbqypvj-shard-00-02.f8ibx0h.mongodb.net:27017/?ssl=true&replicaSet=atlas-6t91cm-shard-0&authSource=admin&retryWrites=true&w=majority";
-
+/*
 MongoClient.connect(uri, function(err, db) {
   var cursor = db.collection("sampledb").find({"name": 1});
     var ret = [];
@@ -52,7 +52,7 @@ MongoClient.connect(uri, function(err, db) {
       //if(doc != null)
         //ret.push(doc);
       //else
-        callback(ret);
+        //callback(ret);
       
   //const collection = client.db("sampledb").collection("rooms");
   // perform actions on the collection object
@@ -63,7 +63,7 @@ MongoClient.connect(uri, function(err, db) {
 
 });
 
-
+*/
 
 var mongoDB     = require('mongodb').MongoClient;
 
