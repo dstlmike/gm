810b09628613ceadd3502030b75dae498d1f7c7e
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-26 00:16:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var db           = require('./modules/db.js');
 var mods         = require('./modules/mods.js');
 var commandList  = require('./modules/command-list.js');
 var rooms        = require('./modules/rooms.js');
-
+//
 //commands with custom actions
 var userCmds     = require('./custom_commands/user-commands.js');
 var userMentions = require('./custom_commands/user-mentions.js');
