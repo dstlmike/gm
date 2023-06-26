23f8e93758a3250bb31b6e2d4af7778443566850
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-25 20:08:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -103,6 +103,7 @@ function postMessage(botResponse, attachments, botID) {
 
   botReq = HTTPS.request(options, function(res) {
       if (res.statusCode == 202 || res.statusCode == 200) {
+        console.log(res);
         //neat
       } else {
         console.log('rejecting bad status code ' + res.statusCode);
